aa868dd5ea49c9825d0ff02c2df6f6f23b918e9b
younes <benjdidawiyad@gmail.com>
2024-11-30 01:57:23 +0100

    adding delete function

--- src/index.js
+++ src/index.js
@@ -77,21 +77,21 @@ function gettactic() {
 }
 
 
-function addchanges(){
+// function addchanges(){
     
-    for(let i=0; i<5 ;i++){
+//     for(let i=0; i<5 ;i++){
     
-        let div2 = `<div id="changesimg" class="">
-        <img class="cardimage" src="./src/assets/Icons/placeholder-card-normal.png" alt="">
-        <div class="plusicon">
-            <svg class="" width="30%" height="30%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#0BB04D"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9 12H12M15 12H12M12 12V9M12 12V15" stroke="#0BB04D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M11.7 1.1732C11.8856 1.06603 12.1144 1.06603 12.3 1.17321L21.2263 6.3268C21.4119 6.43397 21.5263 6.63205 21.5263 6.84641V17.1536C21.5263 17.3679 21.4119 17.566 21.2263 17.6732L12.3 22.8268C12.1144 22.934 11.8856 22.934 11.7 22.8268L2.77372 17.6732C2.58808 17.566 2.47372 17.3679 2.47372 17.1536V6.84641C2.47372 6.63205 2.58808 6.43397 2.77372 6.32679L11.7 1.1732Z" stroke="#0BB04D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-        </div>
-    </div>`
+//         let div2 = `<div id="changesimg" class="">
+//         <img class="cardimage" src="./src/assets/Icons/placeholder-card-normal.png" alt="">
+//         <div class="plusicon">
+//             <svg class="" width="30%" height="30%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#0BB04D"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9 12H12M15 12H12M12 12V9M12 12V15" stroke="#0BB04D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M11.7 1.1732C11.8856 1.06603 12.1144 1.06603 12.3 1.17321L21.2263 6.3268C21.4119 6.43397 21.5263 6.63205 21.5263 6.84641V17.1536C21.5263 17.3679 21.4119 17.566 21.2263 17.6732L12.3 22.8268C12.1144 22.934 11.8856 22.934 11.7 22.8268L2.77372 17.6732C2.58808 17.566 2.47372 17.3679 2.47372 17.1536V6.84641C2.47372 6.63205 2.58808 6.43397 2.77372 6.32679L11.7 1.1732Z" stroke="#0BB04D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+//         </div>
+//     </div>`
   
-    document.getElementById("changes").innerHTML +=(div2)
-    }
-}
-addchanges()
+//     document.getElementById("changes").innerHTML +=(div2)
+//     }
+// }
+// addchanges()
 // addchanges()
 fetch("./src/assets/players.json")
 .then(response => response.json())
@@ -100,64 +100,90 @@ let array ;
 function showplayers(playersinfo){
      array = playersinfo
      console.log(array)
-    for(let ele of playersinfo){
+}    
         
-        let jsoninfoplayer  = `   <div class="imageinfocontainer">
-        <img class="mainimage" src="./src/assets/Icons/badge_total_rush.webp" alt="">
-        <img class="playerimage"  src="${ele.photo}" alt="">
-        <div class="allinfoplayer">
-            <div class="playerpower">
-                <h1>${ele.rating}</h1>
-                <p>${ele.position}</p>
-                <h2>+++</h2>
-            </div>
-            <div class="statistics">
-                <div>
-                    <h3>${ele.passing ? "PAS" : "DIV"}</h3>
-                    <h4>${ele.pace || ele.diving}</h4>
-                </div>
-                <div>
-                    <h3>${ele.defending ? "SHO" : "HAN"}</h3>
-                    <h4>${ele.shooting || ele.handling}</h4>
-                </div>
-                <div>
-                    <h3>${ele.defending ? "PAS" : "KIC"}</h3>
-                    <h4>${ele.passing ||ele.kicking}</h4>
-                </div>
-                <div>
-                    <h3>${ele.defending ? "DRI" : "REF"}</h3>
-                    <h4>${ele.dribbling ||ele.reflexes}</h4>
-                </div>
-                <div>
-                    <h3>${ele.defending ? "DEF" : "SPE"}</h3>
-                    <h4>${ele.defending ||ele.speed}</h4>
-                </div>
-                <div>
-                    <h3>${ele.diving ? "PHY" : "POS"}</h3>
-                    <h4>${ele.positioning || ele.physical}</h4>
-                </div>   
-            </div>
-            <div class="kamalimage">
-                <img class="kamal" src="${ele.flag}" alt="">
-                <img class="kamal" src=""${ele.logo}"" alt="">
+    //     let jsoninfoplayer  = `   <div class="imageinfocontainer">
+    //     <img class="mainimage" src="./src/assets/Icons/badge_total_rush.webp" alt="">
+    //     <img class="playerimage"  src="${ele.photo}" alt="">
+    //     <div class="allinfoplayer">
+    //         <div class="playerpower">
+    //             <h1>${ele.rating}</h1>
+    //             <p>${ele.position}</p>
+    //             <h2>+++</h2>
+    //         </div>
+    //         <div class="statistics">
+    //             <div>
+    //                 <h3>${ele.passing ? "PAS" : "DIV"}</h3>
+    //                 <h4>${ele.pace || ele.diving}</h4>
+    //             </div>
+    //             <div>
+    //                 <h3>${ele.defending ? "SHO" : "HAN"}</h3>
+    //                 <h4>${ele.shooting || ele.handling}</h4>
+    //             </div>
+    //             <div>
+    //                 <h3>${ele.defending ? "PAS" : "KIC"}</h3>
+    //                 <h4>${ele.passing ||ele.kicking}</h4>
+    //             </div>
+    //             <div>
+    //                 <h3>${ele.defending ? "DRI" : "REF"}</h3>
+    //                 <h4>${ele.dribbling ||ele.reflexes}</h4>
+    //             </div>
+    //             <div>
+    //                 <h3>${ele.defending ? "DEF" : "SPE"}</h3>
+    //                 <h4>${ele.defending ||ele.speed}</h4>
+    //             </div>
+    //             <div>
+    //                 <h3>${ele.diving ? "PHY" : "POS"}</h3>
+    //                 <h4>${ele.positioning || ele.physical}</h4>
+    //             </div>   
+    //         </div>
+    //         <div class="kamalimage">
+    //             <img class="kamal" src="${ele.flag}" alt="">
+    //             <img class="kamal" src=""${ele.logo}"" alt="">
                 
-            </div> 
-        </div>    
-    <div>
-    </div>
-    </div>`
-    document.getElementById("addplayertoteam").innerHTML += jsoninfoplayer
+    //         </div> 
+    //     </div>    
+    // <div>
+    // </div>
+    // </div>`
+function allplayers(){
+    
+    document.getElementById("allplayers").innerHTML  = ""
+    for(let ele of array){
+    let jsoninfoplayer = `<div id="selectedplayer" class="imageinfocontainer" >
+                            <img class="mainimage" src="./src/assets/Icons/badge_ballon_dor.webp" alt="" onclick="showmoreinfo(this)">
+                            <img class="playerimage"  src="${ele.photo}" alt="" >
+                            <div class="allinfoplayer">
+                            <div class="playerpower">
+                            <h1>${ele.rating}</h1>
+                            <p>${ele.position}</p>
+                            <h2>+++</h2>
+                            </div>
+                            <div class="statistics">
+                            <h1>${ele.name}</h1> 
+                            </div> 
+                            <div>
+                            </div>
+                            </div>
+                            <div>
+                            <button class="button" onclick="modification1(this)">moddify</button>
+                            <button class="button" onclick="deleteplayer2(this)">delete</button>
+                            </div>
+                            `
+    document.getElementById("allplayers").innerHTML += jsoninfoplayer
 
         
     }
+}
+function deleteplayer2(e){
+    e.closest("#selectedplayer").remove()
+    deleteplayer(e.closest("#selectedplayer").children[2].children[1].children[0].innerHTML)
 }
 let selected = false ; 
 let all1 ;
 let ele1 ;
 let postionforplayer;
 function showselectedplayer(element1){  
-
-   
     ele1 = element1.parentElement.id   
     postionforplayer = element1.parentElement
     document.getElementById("addplayertoteam").innerHTML = ""
@@ -166,8 +192,10 @@ function showselectedplayer(element1){
     console.log(element1.nextElementSibling.innerHTML)
     for(let ele of array){
     if(ele.position == element1.nextElementSibling.innerHTML){
-        
-       
+        let backgroundphoto ;
+       if(ele.rating > 75 ){
+        backgroundphoto
+       }
     let jsoninfoplayer1   = `<div id="selectedplayer" class="imageinfocontainer" >
                             <img class="mainimage" src="./src/assets/Icons/badge_ballon_dor.webp" alt="" onclick="showmoreinfo(this)">
                             <img class="playerimage"  src="${ele.photo}" alt="" >
@@ -182,7 +210,8 @@ function showselectedplayer(element1){
                             </div> 
                             <div>
                             </div>
-                            </div>`
+                            </div>
+                            `
         document.getElementById("addplayertoteam").innerHTML += jsoninfoplayer1
         }
     }
@@ -194,7 +223,7 @@ document.getElementById("addplayertoteam").addEventListener("click",function(e){
                 document.getElementById(ele1).innerHTML = ""
                 document.getElementById(ele1).appendChild(e.target.closest(".imageinfocontainer"))
                 
-                deletepleyer(e.target.closest(".imageinfocontainer"))
+                deleteplayer(e.target.closest(".imageinfocontainer").children[2].children[1].children[0].innerHTML)
                 console.log(document.getElementById(ele1).style.gridArea)
                 positionimage = ` <div id="position"> 
                                   </div>`
@@ -204,6 +233,7 @@ document.getElementById("addplayertoteam").addEventListener("click",function(e){
                 document.getElementById(ele1).appendChild(postiondeplayer)
                 selected = false 
         }
+  
 })
 // function drageanddrop(dragethisplayer) {
 //     let palyerdrope = document.querySelectorAll(".player");
@@ -283,24 +313,33 @@ document.getElementById("addplayertoteam").addEventListener("click",function(e){
     
             
 let  infoarray = [];       
-function deletepleyer(deletitem){
+function deleteplayer(deletitem){
    
     let  newarray1 = array.filter((item1)=> {
-        if(item1.name != deletitem.children[2].children[1].children[0].innerHTML) return item1 ;
+        if(item1.name != deletitem) return item1 ;
         infoarray.push(item1);
     })
     
     array = newarray1
 }
-// document.querySelector("imageinfocontainer").addEventListener('dragstart',function(e){
-//     console.log("kamal")
-// })
+function reviveplayer(player){
+    array = infoarray.filter(item => {
+        if(item.name == player) array.push(item); 
+        
+    }) 
+}
 function draganddrop(){
 
     }
+let swetchedplayer;
+let selectedswitchplayer ;
 function showmoreinfo(item){
+    
+    selectedswitchplayer = true 
+    swetchedplayer = item
+
     console.log(infoarray)
-    console.log("kamal")
+    
     console.log(item.parentElement.children[2].children[1].children[0].innerHTML )
     
         
@@ -308,7 +347,6 @@ function showmoreinfo(item){
             
        
         if(item.parentElement.children[2].children[1].children[0].innerHTML == ele.name){
-            console.log("younes")
             let diveitem  = `<div class="allplayerinfo">
             <div>
             <h1 class="close-icon" onclick="close1()">X</h1>
@@ -323,7 +361,6 @@ function showmoreinfo(item){
             <img src="${ele.flag}" alt="flagimage" class="flagimage">
             <img src="${ele.club}" alt="teamimage" class="team-imag">
             </div>
-            
             </div>  
             </div>
             <d class="power-info" >
@@ -352,14 +389,36 @@ function showmoreinfo(item){
 
     }
 }
+document.getElementById("addplayertoteam").addEventListener("click",function(e){
+    if(selectedswitchplayer){   
+        let selectedelement = e.target.closest(".imageinfocontainer").children
+        document.getElementById(ele1).innerHTML = ""
+        document.getElementById(ele1).appendChild(e.target.closest(".imageinfocontainer"))
+        
+        deleteplayer(e.target.closest(".imageinfocontainer").children[2].children[1].children[0].innerHTML)
+        console.log(e.target.closest(".imageinfocontainer"))
+        console.log(document.getElementById(ele1).style.gridArea)
+        positionimage = ` <div id="position"> 
+                          </div>`
+        let postiondeplayer = document.createElement("div")
+        postiondeplayer.id = "position_1"
+        postiondeplayer.innerHTML = `${document.getElementById(ele1).style.gridArea.slice(0,2).toUpperCase()}` 
+        document.getElementById(ele1).appendChild(postiondeplayer)
+        selectedswitchplayer = false 
+        console.log(ele1 ,"kamal")
+        console.log(swetchedplayer.closest(".imageinfocontainer").children[2].children[1].children[0].innerHTML)
+}
+
+})
 function close1(){
-    console.log("kakasc")
+    
     document.querySelector(".eria-info").style.display = "none"
     document.querySelector(".inputs-info").style.display = "block"
 }
 function position() {
+    let positon
     if(document.getElementById("positionshowsed").value == "GK"){
-        let positongk = `   
+         positon = `   
                     
                           
                         <label for="Diving">Diving : </label>
@@ -374,7 +433,23 @@ function position() {
                         <input type="range" id="speed" name="" min="0" max="100" step="10"/>
                         <label for="positioning">positioning : </label>
                         <input type="range" id="positioning" name="" min="0" max="100" step="10"/>` 
-                        document.getElementById("switchposition").innerHTML = positongk 
+                        document.getElementById("switchposition").innerHTML = positon
+    }
+    else {
+        positon = `<label for="pace">pace : </label>
+        <input type="range" id="pace" name="" min="0" max="100"  step="10"/>
+        <label for="Shooting">Shooting : </label>
+        <input type="range" id="shooting" name="" min="0" max="100"  step="10"/>
+        <label for="passing">passing : </label>
+        <input type="range" id="passing" name="" min="0" max="100"  step="10"/>
+        <label for="dribbling">dribbling : </label>
+        <input type="range" id="dribbling" name="" min="0" max="100"step="10"/>
+        <label for="defending">defending : </label>
+        <input type="range" id="defending" name="" min="0" max="100" step="10"/>
+        <label for="physical"> physical : </label>
+        <input type="range" id="physical" name="" min="0" max="100" step="10"/>  `
+        document.getElementById("switchposition").innerHTML = positon
+
     }
 }
 let playeronobject ;
@@ -396,7 +471,8 @@ document.getElementById("addplayer").addEventListener("click",function(e){
         "kicking": document.getElementById("kicking").value,
         "reflexes": document.getElementById("reflexes").value,
         "speed": document.getElementById("speed").value ,
-        "positioning": document.getElementById("positioning").value
+        "positioning": document.getElementById("positioning").value,
+        "id" : "1"
     }
 }
     else {
@@ -414,9 +490,9 @@ document.getElementById("addplayer").addEventListener("click",function(e){
         "passing": document.getElementById("passing").value,
         "dribbling": document.getElementById("dribbling").value,
         "defending": document.getElementById("defending").value,
-        "physical": document.getElementById("physical").value
+        "physical": document.getElementById("physical").value,
+        "id" : "1"  
         }
-
     }
     array.push(playeronobject)
 
